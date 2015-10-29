3589f1d900774b740a10b81a6647ff043eec678d
abudexiong <qmwk1120@gmail.com>
2015-10-30 00:29:01 +0800

    day1
    day1

---
+++ practice1-5.py
@@ -0,0 +1,86 @@
+# -*- coding:utf-8 -*-
+print"Hello World"
+print"Hello Again"
+print "I like typing this."
+print "This is fun"
+print "Ya! Printing"
+print "I'd much rather you yet"
+print "I will said do not touch this\n"
+
+#part2
+# A comment,this is so you can read your program later.
+# Anything after the # is ignored by python.
+
+print"I could have code like this"#and the comment after is ignored.
+
+#you can also use a comment to"disabled" or comment out a piece of code.
+#print "This won't run."
+
+print"This will run.\n"
+
+
+#part 3
+print"I will now count my chickens."
+
+print"Hans",25+30/6
+print "Roostar,",100-25*3%4
+
+print "Now I will count the eggs:"
+
+print 3+2+1-5+4%2-1/4+6
+
+print"Is it true that 3+2<5-7?"
+
+print 3+2<5-7
+
+print"what is 3+2?",3+2
+print"what is 5-7?",5-7
+
+print"Oh That's why it's false"
+
+print"how about some more."
+
+print"Is it greater?",5>-2
+print"Is it greater or equal?",5>=-2
+print "Is it less or equal?",5<=-2,"\n"
+
+#part4
+cars=100
+space_in_a_car=4.0
+drivers=30
+passengers=90
+cars_not_driven=cars-drivers
+cars_driven=drivers
+carpool_capacity=cars_driven*space_in_a_car
+average_passengers_per_car=passengers/cars_driven
+
+print"There are",cars,"cars available."
+print"There are only",drivers,"drivers avaiable"
+print"There will be",cars_not_driven,"empty cars today"
+print"We can transport",carpool_capacity,"people today"
+print "We have",passengers,"to carpool today"
+print"We need to put about",average_passengers_per_car,"in each car\n"
+
+
+#part 5
+my_name="Zed A.Shaw"#String we can use"or '.
+my_age=35# not a lie
+my_heigh=74# inches
+my_weight=180# lbs
+my_eyes='Blue'
+my_teeth='White'
+my_hair='Brown'
+
+print"Lets talk about %s."% my_name
+print "He is %d inches tall."% my_heigh
+print "He is %d pounds weight"% my_weight
+print"Actually he is not too heavy"
+print "He got %s eyes and %s hair."%(my_eyes,my_hair)
+print"His teeth are usually %s depending on the coffee."%my_teeth
+
+#this line is tricky,try to get it exactly right
+print"if I add %d,%d,and%d I get &d"%(my_age,my_heigh,my_weight,my_age+my_weight+my_heigh)
+
+
+
+
