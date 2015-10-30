6522332b3efa1d78feda97500a7c7d5a9dd2c798
abudexiong <qmwk1120@gmail.com>
2015-10-30 10:55:24 +0800

    day2
    day2

---
+++ practice13.py
@@ -0,0 +1,11 @@
+#-*- coding:utf-8 -*-
+__author__ = '凯'
+from sys import argv
+
+script,first,second,third=argv
+
+print "The script is called:",script
+print "your first variable is:",first
+print "your second variable is:",second
+print "your third variable is:",third
+
